8982fad724e6bbdb8c4dbf05735689a30dd84960
EDnotSheeran <juninho.tab3@gmail.com>
2020-08-17 16:02:44 -0300

    mudança no logo do servidor

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,14 @@ import routes from './routes';
 require('dotenv').config();
 
 const TOKEN = process.env.TOKEN;
+const PORT = process.env.SERVER_PORT;
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
-console.log("\n>server OK - Sem erros detectados, até o momento\n");
+app.listen(PORT,()=>{
+    console.log('\x1b[33m%s\x1b[0m',`>iniciando na porta ${PORT}...\n>server OK  - Sem erros detectados, até o momento\n`);
+});
